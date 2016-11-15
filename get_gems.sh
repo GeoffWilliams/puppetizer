c1193d4e0957a065754e8069536346d21b99cde8
Geoff Williams <geoff.williams@puppetlabs.com>
2016-11-15 16:17:34 +0800

    alternate gem download method

--- get_gems.sh
+++ get_gems.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
 # temporary script to download the gems we want for offline usage 
-# into a directory called 'gems'
+# into a directory called 'gems/cache'
+mkdir -p gems/cache
+pushd gems/cache
 
-gem install puppetclassify -i gems --no-rdoc --no-ri
-gem install pe_rbac -i gems --no-rdoc --no-ri
-gem install hiera-eyaml -i gems --no-rdoc --no-ri
+for GEM in  domain_name \
+            escort \
+            hiera-eyaml \
+            highline \
+            http-cookie \
+            mime-types \
+            mime-types-data \
+            nesty \
+            netrc \
+            pe_rbac \
+            puppetclassify \
+            rest-client \
+            trollop \
+            unf \
+            unf_ext ; do
+  gem fetch $GEM
+done
+
+#gem install puppetclassify -i gems --no-rdoc --no-ri
+#gem install pe_rbac -i gems --no-rdoc --no-ri
+#gem install hiera-eyaml -i gems --no-rdoc --no-ri
 
 
 
