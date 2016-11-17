72b00df4bf53b9d542451f571c6c77aa2ff1c998
Geoff Williams <geoff.williams@puppetlabs.com>
2016-11-17 23:49:50 +0800

    licence info

--- get_gems.sh
+++ get_gems.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# temporary script to download the gems we want for offline usage 
+# temporary script to download the gems we want for offline usage
 # into a directory called 'gems/cache'
 mkdir -p gems/cache
 pushd gems/cache
@@ -22,11 +22,3 @@ for GEM in  domain_name \
             unf_ext ; do
   gem fetch $GEM
 done
-
-#gem install puppetclassify -i gems --no-rdoc --no-ri
-#gem install pe_rbac -i gems --no-rdoc --no-ri
-#gem install hiera-eyaml -i gems --no-rdoc --no-ri
-
-
-
-
